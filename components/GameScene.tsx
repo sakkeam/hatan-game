c18ea3a04e6c781b712dc0146cd3d5fda72997d6
Konosuke Sakai <konosukeam@gmail.com>
2025-11-10 19:26:56 +0900

    fix: remove lane guidelines and ground reference from Scene component

--- components/GameScene.tsx
+++ components/GameScene.tsx
@@ -36,18 +36,6 @@ function Scene() {
       {goal && !goal.reached && (
         <Goal goal={goal} />
       )}
-      {/* Lane guidelines */}
-      {LANES.map((laneY, index) => (
-        <mesh key={`lane-${index}`} position={[0, laneY, -1]} rotation={[0, 0, 0]}>
-          <boxGeometry args={[50, 0.05, 0.1]} />
-          <meshStandardMaterial color="#4a5568" opacity={0.3} transparent />
-        </mesh>
-      ))}
-      {/* Ground reference */}
-      <mesh position={[0, -6, -2]} rotation={[-Math.PI / 2, 0, 0]}>
-        <planeGeometry args={[50, 20]} />
-        <meshStandardMaterial color="#2d3748" opacity={0.3} transparent />
-      </mesh>
     </>
   );
 }
